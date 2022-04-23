c41aa4774829f878a9cfe25a1254375abc9d30cd
Alejandro Mejia <alejomejiacuartas@outlook.com>
2022-04-22 20:45:15 -0500

    add theme support to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 
 import GlobalCSS from 'utils/styled/global'
+import theme from 'utils/styled/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalCSS />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
